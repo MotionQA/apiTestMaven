e0cc1508b1c661f54b7cb1fa080f7059cef2e63a
MotionQA <keygo@mail.ru>
2026-09-11 20:10:36 +0300

    fakerestapi success!

--- src/test/java/SmokeApiTests.java
+++ src/test/java/SmokeApiTests.java
@@ -4,30 +4,28 @@ import org.junit.Test;
 import static io.restassured.RestAssured.given;
 
 public class SmokeApiTests {
-    private static final String BASE_URL = "http://localhost:8080";
+    private static final String BASE_URL = "https://fakerestapi.azurewebsites.net/api/v1/";
 
     @Test
     public void createUserTest(){
         String bodyJson;
         bodyJson = """
                 {
-                "id": 0,
-                "username": "string",
-                "firstName": "string",
-                "lastName": "string",
-                "email": "string",
-                "password": "string",
-                "phone": "string",
-                "userStatus": 0,
-                }""";
+                  "id": 0,
+                  "userName": "string",
+                  "password": "string"
+                }
+               
+                """;
 
         ValidatableResponse response = given()
-                .header( "accept", "application/json")
+                .header( "accept", " */*")
                 .header( "Content-Type" ,"application/json")
                 .body(bodyJson)
-                .when().post(BASE_URL )
+                .when().post(BASE_URL + "Users" )
                 .then();
-        response.statusCode(200);
+        response.statusCode(200
+        );
         response.extract().response().prettyPrint();
     }
 }
